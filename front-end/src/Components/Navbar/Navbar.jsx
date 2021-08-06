ea0b71e1320c720ec5140898d592c75e1a00f79e
Amol Talekar <amol11talekar@gmail.com>
2021-08-06 17:05:44 +0530

    folder structure

---
+++ front-end/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+            <h1>This is Navabr</h1>
+            
+        </div>
+    )
+}
+
+export {Navbar}
---
+++ front-end/src/Routes/Routes.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Routes = () => {
+    return (
+        <div>
+            <h1>This is Routes</h1>
+        </div>
+    )
+}
+
+export {Routes}
