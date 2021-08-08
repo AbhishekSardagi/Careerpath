a31c8a8f8e91ef495f8f168b1995fb1c0b8cb799
amankumarsuman <amankumarsuman.ssjha143@gmail.com>
2021-08-08 12:49:20 +0530

    updated

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,4 @@
 import "./App.css";
-import Chat from "./Components/Chat";
 import Footer from "./Components/Footer/Footer";
 import Header from "./Components/Header/Header";
 import { Routes } from "./Routes/Routes";
