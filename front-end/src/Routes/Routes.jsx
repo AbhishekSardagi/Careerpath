60db095c466f66a8d0f694eb0ccfae5dc492b6cc
minakshidast <minakshid606@gmail.com>
2021-08-08 12:43:21 +0530

    final commit

--- front-end/src/Routes/Routes.jsx
+++ front-end/src/Routes/Routes.jsx
@@ -5,13 +5,20 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Home } from "../Components/Homepage/Home";
 import { StageOne } from "../Components/StageOne/StageOne";
 import { SingleCourse } from "../Components/SingleCourse/SingleCourse";
+import { Engineer } from "../Components/Engineer/Engineer";
+import { YourPath } from "../Components/YourPath/YourPath";
 const Routes = () => {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/stageone" component={StageOne} />
-        <Route path="/stageone/singlecourse" component={SingleCourse} />
+        <Route exact path="/stageone/singlecourse" component={SingleCourse} />
+        <Route exact path="/stageone/singlecourse/eng" component={Engineer} />
+        <Route
+          path="/stageone/singlecourse/eng/yourpath"
+          component={YourPath}
+        />
       </Switch>
     </div>
   );
